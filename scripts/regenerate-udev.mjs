3972a588df4cc227f39ce530277e99f0f4e44200
Julian Waller <git@julusian.co.uk>
2025-05-31 00:50:42 +0100

    fix: tool to generate udev rules

---
+++ scripts/regenerate-udev.mjs
@@ -0,0 +1,39 @@
+/* eslint-disable n/no-extraneous-import */
+// @ts-check
+
+import fs from 'node:fs/promises'
+
+import { UdevRuleGenerator } from 'udev-generator'
+import { DEVICE_MODELS2, VENDOR_ID } from '@blackmagic-controller/core'
+
+const generator = new UdevRuleGenerator()
+
+for (const model of Object.values(DEVICE_MODELS2)) {
+	for (const productId of model.productIds) {
+		if (productId >= 0xfff0) continue // Skip reserved product IDs
+		generator.addDevice(VENDOR_ID, productId)
+	}
+}
+
+const headlessStr = generator.generateFile({
+	mode: 'headless',
+})
+const desktopStr = generator.generateFile({
+	mode: 'desktop',
+})
+
+const rulesJSON = JSON.stringify(generator.rules, undefined, '\t')
+
+await fs.writeFile(new URL('../packages/node/udev/50-blackmagic-panel-user.rules', import.meta.url), desktopStr, 'utf8')
+await fs.writeFile(
+	new URL('../packages/node/udev/50-blackmagic-panel-headless.rules', import.meta.url),
+	headlessStr,
+	'utf8',
+)
+
+await fs.writeFile(new URL('../packages/web/udev/50-blackmagic-panel-user.rules', import.meta.url), desktopStr, 'utf8')
+
+await fs.writeFile(new URL('../packages/node/udev-generator-rules.json', import.meta.url), rulesJSON, 'utf8')
+await fs.writeFile(new URL('../packages/web/udev-generator-rules.json', import.meta.url), rulesJSON, 'utf8')
+
+console.log('Udev rules regenerated successfully!')
